aad0f7649d11062b2411aa8131658467819aab95
adalidcasas <cas19040@byui.edu>
2025-01-15 13:03:45 -0400

    Excercise 4 w1 finished

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int num = -1;
+        while (num != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num != 0)
+            {
+                numbers.Add(num);
+            }
+        }
+
+        int sum = 0;
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The max is: {max}");
     }
 }
