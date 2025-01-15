bc2f12a2abd345c0b8a78cb7d9ed0938611328f8
adalidcasas <cas19040@byui.edu>
2025-01-15 12:43:06 -0400

    Excercise 2 week 2 finished

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string gradeletter = "";
+
+        if (percent >= 90)
+        {
+            gradeletter = "A";
+        }
+        else if (percent >= 80)
+        {
+            gradeletter = "B";
+        }
+        else if (percent >= 70)
+        {
+            gradeletter = "C";
+        }
+        else if (percent >= 60)
+        {
+            gradeletter = "D";
+        }
+        else
+        {
+            gradeletter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {gradeletter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
