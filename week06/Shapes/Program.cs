d652f9b9e533fa2f7b7e67567516e832c3e093c0
adalidcasas <cas19040@byui.edu>
2025-02-18 16:36:45 -0400

    Excercise Shapes, weeek 6. Complete

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear();
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("blue", 2);
+        shapes.Add(square);
+        Rectangle rectangle = new Rectangle("yellow", 4, 3);
+        shapes.Add(rectangle);
+        Circle circle = new Circle("green", 6);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
